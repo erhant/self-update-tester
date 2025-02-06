42c6a8308c5f01b9bbdc71eba39e5c4db06a5192
erhant <erhany96@gmail.com>
2025-02-06 19:32:11 +0300

    update dummy

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-/// All i do is add, and return my version!
+/// All i do is add.
 pub fn add_stuff(a: i32, b: i32) -> i32 {
     a + b
+}
+
+/// All i do is subtract.
+pub fn sub_stuff(a: i32, b: i32) -> i32 {
+    a - b
 }
